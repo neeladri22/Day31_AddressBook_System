72ef10dd69ae339d7374dee3b7fcee757394384a
neeladri22 <neeladri22@gmail.com>
2022-07-24 02:16:00 -0400

    Create Address Book system Database

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create AddreessBook_System Database
+            CreateAddressBookServiceDB();
+        }
+        //Create New Database
+        public static void CreateAddressBookServiceDB()
+        {
+            string SQL = "CREATE DATABASE AddreessBook_System ";
+
+            string connectingstring = @"Server=localhost;Integrated security=SSPI;database=master";
+
+            SqlConnection connection = new SqlConnection(connectingstring);
+            SqlCommand cmd = new SqlCommand(SQL, connection);
+            try
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Database Created Successfully....");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception :" + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+    }
+}
+
