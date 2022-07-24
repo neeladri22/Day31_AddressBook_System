d53bea2b68d11d9310fce66c0ef9613b76a71b06
neeladri22 <neeladri22@gmail.com>
2022-07-24 02:40:53 -0400

    Insert New Contacts in AddressBook Table

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -10,8 +10,11 @@ namespace AddressBookSystem
             //Create AddreessBook_System Database
             CreateAddressBookServiceDB();
 
-            //Create AddressBook Table-UC2
-            CreateAddressBookTable(); 
+            //Create AddressBook Table
+            CreateAddressBookTable();
+
+            //Insert New Contacts in  AddressBook Table
+            InsertContactInAddressBookTable();
         }
         //Create New Database
         public static void CreateAddressBookServiceDB()
@@ -52,6 +55,24 @@ namespace AddressBookSystem
             Console.WriteLine("AddressBook Table Created Successfully");
             Console.ReadKey();
         }
+
+        //Insert New Contacts in  AddressBook Table
+        public static void InsertContactInAddressBookTable() //UC3
+        {
+            var SQL = @$"INSERT INTO AddressBook Values  ('Neeladri','Pulakala','PL puram','Ichapuram', 'Andhra','123654','8965741236','neela@gmail.com'),
+                                                         ('Usha','Pulakala','Ichapuram','Ichapuram', 'Visaka','789456','8547962314','usha@gmail.com'),
+                                                         ('Varsa','Pulakala','Donkuru','Donkuru', 'Visaka','456321','741852963','varsa@gmail.com');";
+           
+            string connectingString = @"Data Source=DESKTOP-2UKFQA8;Initial Catalog=AddreessBook_System;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectingString);
+            SqlCommand cmd = new SqlCommand(SQL, connection);
+            connection.Open();
+            int reader = cmd.ExecuteNonQuery();
+            Console.WriteLine(reader);
+            Console.WriteLine("New contact is Successfully inserted");
+            Console.ReadKey();
+        }
+
     }
 }
 
