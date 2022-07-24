ba3b45899f2f62e3a3e5a8c77d0d3bdb326e18f9
neeladri22 <neeladri22@gmail.com>
2022-07-24 03:38:00 -0400

    Get number of contact persons i.e count by type-U10

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -41,7 +41,8 @@ namespace AddressBookSystem
 
             //Identify the address book by Name and Type
             UpdateAddressBookByNameandType(); //UC9-extended
-
+            //Get number of contact persons i.e count by type-U10
+            GetNumberOfPersonsCountbyType();
 
         }
         //Create New Database
@@ -224,6 +225,28 @@ namespace AddressBookSystem
             int reader = cmd.ExecuteNonQuery();
             Console.WriteLine(reader);
             Console.WriteLine("AddressBook Table is Updated Successfully ");
+            Console.ReadKey();
+        }
+
+        //Get number of contact persons i.e count by type
+        public static void GetNumberOfPersonsCountbyType()
+        {
+            var SQL = @$"SELECT AddressBookType,  COUNT(AddressBookType) from AddressBook group by AddressBookType";
+            string connectingString = @"Data Source=DESKTOP-2UKFQA8;Initial Catalog=AddreessBook_System;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectingString);
+            SqlCommand cmd = new SqlCommand(SQL, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("AddressBook Type {0}  - Numbers {1} ", reader["AddressBookType"], reader);
+                }
+                reader.Close();
+            };
+            Console.WriteLine("\nRetrieve data Successfully");
             Console.ReadKey();
         }
 
