8a6811c61a3138462d6ce1a3dd0ed25c13ad0609
neeladri22 <neeladri22@gmail.com>
2022-07-24 00:31:33 -0400

    Create address book table in database

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -3,3 +3,20 @@
 CREATE DATABASE Address_Book_Service
 
 use Address_Book_Service
+
+--Create AddressBook Table
+CREATE TABLE AddressBook
+(
+FirstName Varchar(20),
+LastName Varchar(15),
+Address Varchar(50),
+City Varchar(20),
+State Varchar(20),
+ZIP int,
+PhoneNumber Varchar(15),
+Email Varchar(20)
+)
+
+--To View the AddressBook Table
+SELECT * FROM AddressBook
+Footer
